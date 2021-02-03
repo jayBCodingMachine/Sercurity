b6eb78cb230567344bcd062a18e23899c2eaccf3
Jabulani Nyawera <jaybcoding@yahoo.com>
2021-02-02 19:11:06 -0600

    change security to bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const app = express();
 
 
@@ -42,11 +43,13 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    // Store hash in your password DB.
     const newUser = new User({
       email: req.body.username,
-      password: md5(req.body.password)
+      password: hash
     });
-
     newUser.save(function(err){
       if (err){
         console.log(err);
@@ -57,18 +60,28 @@ app.post("/register", function(req, res){
 
 });
 
+
+});
+
 app.post("/login", function(req, res){
 const username = req.body.username;
-const password = md5(req.body.password);
+const password = req.body.password;
 
 User.findOne({email: username}, function(err, foundUser){
   if (err){
     console.log(err);
   } else {
       if(foundUser){
-          if(foundUser.password === password){
-            res.render("secrets")
+        bcrypt.compare(password, foundUser.password, function(errs, result) {
+          if (result === true){
+            res.render("secrets");
+          } else {
+            console.log(errs);
           }
+    // result == true
+          });
+
+
       }
   }
 
